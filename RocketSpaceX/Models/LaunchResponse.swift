64fc286fc08ddde5e508e56e8f816045c3421a30
hariharanjneuralrays <75111331+hariharanjneuralrays@users.noreply.github.com>
2022-05-11 10:05:23 +0100

    first commit

---
+++ RocketSpaceX/Models/LaunchResponse.swift
@@ -0,0 +1,12 @@
+//
+//  LaunchResponse.swift
+//  RocketSpaceX
+//
+//  Created by Hariharan on 09/05/22.
+//
+
+import Foundation
+
+struct LaunchResponse: Codable, Equatable {
+    let docs: [Launch]?
+}
